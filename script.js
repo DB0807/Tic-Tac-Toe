8b9fc4f39d8284e77e7f36698973fd42bb297bab
DB0807 <dylanbradley87@outlook.com>
2024-01-18 20:37:39 +0000

    add create player factory

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ const gameboard = (function () {
     const Gameboard = [,,,,,,,,,]
     return {Gameboard,}
 })();
+
+function createPlayer (name, marker) {
+    const playerName = name
+    const playerMarker = marker
+    return {playerName, playerMarker,}
+}
