0edf91846821dbb33f3e139ccfe5f656407bd4d3
DB0807 <dylanbradley87@outlook.com>
2024-01-22 20:55:56 +0000

    Add row and col win condition

--- script.js
+++ script.js
@@ -1,10 +1,42 @@
 const gameboard = (function () {
-    const Gameboard = [,,,,,,,,,]
+    const Gameboard = ["","","","","","","","",""]
     return {Gameboard,}
 })();
 
 function createPlayer (name, marker) {
     const playerName = name
     const playerMarker = marker
-    return {playerName, playerMarker,}
+    let score = 0
+    const addScore = function () {
+        return this.score += 1
+    }
+    return {playerName, playerMarker, score, addScore,}
 }
+
+const gameFuncs = (function (){
+    const checkWinner = function(player) {
+        for(let i = 0; i <=3; i++){
+          if (gameboard.Gameboard[i] === player.marker && 
+              gameboard.Gameboard[i + 1] === player.marker && 
+              gameboard.Gameboard[i + 2] === player.marker) {
+              return `win on row ${i + 1}`
+            }
+          }
+        for(let i = 0; i <=3; i++){
+        if (gameboard.Gameboard[i] === player.marker && 
+            gameboard.Gameboard[i + 3] === player.marker && 
+            gameboard.Gameboard[i + 6] === player.marker) {
+            return `win on column ${i + 1}`
+            }
+        }
+      }
+    const addMarker = function (boardSpace, mark){
+        return gameboard.Gameboard.splice(boardSpace, 1, mark)
+    }
+    return {checkWinner, addMarker,}
+})();
+
+const playGame = (function () {
+    const playerOne = createPlayer ('Player one', 'X')
+    const playerTwo = createPlayer ('Player two', 'O')
+})();
