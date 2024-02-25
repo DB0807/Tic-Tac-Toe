8f1844561a4957fbbb2b2025c56920c44c5565ff
DB0807 <dylanbradley87@outlook.com>
2024-02-25 18:55:05 +0000

    lots of changes to displayController and click events added to board

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//change gameboared logic thorughout so correct versions of Gameboard and gameboard are used
 const Gameboard = (function () {
     const gameboard = ["","","","","","","","",""]
     const getBoard = () => gameboard
@@ -13,57 +12,58 @@ const gameFuncs = (function (){
         return playerNum === 0 ? playerOne : playerTwo
     }
 
-    const changeActivePlayer = function (currentPlayer, playerOne, playerTwo) {
-        return currentPlayer === playerOne ? playerTwo : playerOne;
-    }
-
     const clearGameboard = function () {
         return gameboard.Gameboard = ["","","","","","","","",""]
     }
 
     const checkWinner = function(player) {
         for(let i = 0; i <3; i++){
-        if (gameboard.Gameboard[i * 3] === player.playerMarker && 
-            gameboard.Gameboard[i * 3 + 1] === player.playerMarker && 
-            gameboard.Gameboard[i * 3 + 2] === player.playerMarker) {
-            gameboard.winner = true
+        if (Gameboard.gameboard[i * 3] === player.playerMarker && 
+            Gameboard.gameboard[i * 3 + 1] === player.playerMarker && 
+            Gameboard.gameboard[i * 3 + 2] === player.playerMarker) {
+            Gameboard.winner = true
             return console.log(`win on row ${i + 1}, ${player.playerName}`)
             }
         }
         for(let i = 0; i <3; i++){
-        if (gameboard.Gameboard[i] === player.playerMarker && 
-            gameboard.Gameboard[i + 3] === player.playerMarker && 
-            gameboard.Gameboard[i + 6] === player.playerMarker) {
-            gameboard.winner = true
+        if (Gameboard.gameboard[i] === player.playerMarker && 
+            Gameboard.gameboard[i + 3] === player.playerMarker && 
+            Gameboard.gameboard[i + 6] === player.playerMarker) {
+            Gameboard.winner = true
             return console.log(`win on column ${i + 1}, ${player.playerName}`)
             }
         }
-        if (gameboard.Gameboard[4] === player.playerMarker && 
-            ((gameboard.Gameboard[0] === player.playerMarker && 
-            gameboard.Gameboard[8] === player.playerMarker) ||
-            (gameboard.Gameboard[2] === player.playerMarker && 
-            gameboard.Gameboard[6] === player.playerMarker))) {
-            gameboard.winner = true
+        if (Gameboard.gameboard[4] === player.playerMarker && 
+            ((Gameboard.gameboard[0] === player.playerMarker && 
+            Gameboard.gameboard[8] === player.playerMarker) ||
+            (Gameboard.gameboard[2] === player.playerMarker && 
+            Gameboard.gameboard[6] === player.playerMarker))) {
+            Gameboard.winner = true
             return console.log(`win on diag, ${player.playerName}`)
             }
         }
 
     const addMarker = function (boardSpace, marker, player){
-        if (boardSpace <= 8 && gameboard.Gameboard[boardSpace] === ""){
-            gameboard.Gameboard.splice(boardSpace, 1, marker)
-            console.log(gameboard.Gameboard)
+        if (boardSpace <= 8 && Gameboard.gameboard[boardSpace] === ""){
+            Gameboard.gameboard.splice(boardSpace, 1, marker)
+            console.log(Gameboard.gameboard)
             return checkWinner(player)
         }
         else return alert('Dont cheat, skip a go!')
     }
-    return {chooseRandomPlayer, 
-            changeActivePlayer, 
+    return {chooseRandomPlayer,  
             clearGameboard, 
             checkWinner, 
             addMarker,}
 })();
 
-const displayLogic = function (){
+const createPlayer = function (name, marker) {
+    const playerName = name
+    const playerMarker = marker
+    return {playerName, playerMarker,}
+};
+
+const displayController = (function () {
     const board = Gameboard.getBoard()
     const boardContainer = document.querySelector('#gameboard-container')
     let spaceNum = 0
@@ -71,28 +71,48 @@ const displayLogic = function (){
         const boardSpace = document.createElement('div')
         boardSpace.dataset.spaceNum = spaceNum
         boardSpace.classList.add('board-space')
+        boardSpace.textContent = Gameboard.gameboard[spaceNum]
         boardContainer.appendChild(boardSpace)
         spaceNum += 1
         return boardSpace
     }
-    render = () => board.forEach(createSpace)
+    const render = () => board.forEach(createSpace)
 
-    return {render,}
-}();
+    const startGame = function () {
+        const playGameBtn = document.querySelector('#play-game')
+        playGameBtn.addEventListener('click', () => {
+            playGame();
+        })
+    }
+
+    return {render, startGame}
+})();
 
 const playGame = function () {
-     const createPlayer = function (name, marker) {
-        const playerName = name
-        const playerMarker = marker
-        return {playerName, playerMarker,}
+
+    const boardSpaces = document.querySelectorAll('.board-space');
+    const updateBoard = function () {
+        boardSpaces.forEach((boardSpace, index) => {
+            boardSpace.textContent = Gameboard.gameboard[index];
+        });
     }
+
     const playerOne = createPlayer(prompt('Enter your name:', 'Player One'), prompt('Choose your marker:','X'))
     const playerTwo = createPlayer(prompt('Enter your name:', 'Player Two'), prompt('Choose your marker:','O'))
     let activePlayer = gameFuncs.chooseRandomPlayer(playerOne, playerTwo)
-    while (Gameboard.winner === false) {
-        gameFuncs.addMarker(prompt(`${activePlayer.playerName}, Choose your square:`), activePlayer.playerMarker, activePlayer)
-        activePlayer = gameFuncs.changeActivePlayer(activePlayer, playerOne, playerTwo)       
+
+    const changeActivePlayer = function (currentPlayer, playerOne, playerTwo) {
+        return currentPlayer === playerOne ? playerTwo : playerOne;
     }
+
+    boardSpaces.forEach(boardSpace => {
+        boardSpace.addEventListener('click', () => {
+        gameFuncs.addMarker(boardSpace.dataset.spaceNum, activePlayer.playerMarker, activePlayer)
+        activePlayer = changeActivePlayer(activePlayer, playerOne, playerTwo)
+        updateBoard()
+        })
+    });
 };
 
-displayLogic.render()
+displayController.render()
+displayController.startGame()
